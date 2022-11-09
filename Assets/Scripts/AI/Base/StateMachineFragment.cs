8ad33e8ef000931733649b727d73c4efb3e85533
vladnovoren <vladnovoren@gmail.com>
2022-11-09 06:19:33 +0300

    StateMachineFragment for roaming & walls prefabs

--- Assets/Scripts/AI/Base/StateMachineFragment.cs
+++ Assets/Scripts/AI/Base/StateMachineFragment.cs
@@ -4,37 +4,40 @@ namespace AI.Base
 {
     public class StateMachineFragment
     {
-        public StateMachineFragment()
+        public StateMachineFragment(State entry)
         {
+            Entry = entry;
             _states = new List<State>();
+            AddState(entry);
         }
 
         public void AddState(State state)
         {
             _states.Add(state);
         }
-
+        
         public void AddTransitionToAllStates(Transition transition)
         {
             foreach (var state in _states)
                 state.AddTransition(transition);
         }
 
-        public static StateMachineFragment operator+(StateMachineFragment fragment1,
-            StateMachineFragment fragment2)
+        public static StateMachineFragment Merge(StateMachineFragment fragment1,
+            StateMachineFragment fragment2, State entry)
         {
-            StateMachineFragment result = new StateMachineFragment();
-            OperatorPlusCore(result, fragment1);
-            OperatorPlusCore(result, fragment2);
+            var result = new StateMachineFragment(entry);
+            MergeCore(result, fragment1);
+            MergeCore(result, fragment2);
             return result;
         }
 
-        private static void OperatorPlusCore(StateMachineFragment result, StateMachineFragment operand)
+        private static void MergeCore(StateMachineFragment result, StateMachineFragment operand)
         {
              foreach (var state in operand._states)
                 result.AddState(state);           
         }
 
+        public State Entry { get; private set; }
         private List<State> _states;
     }
 }
--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,32 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using AI.Base;
+using AI.Roaming;
 
 public class EnemyAI : MonoBehaviour
 {
-    [SerializeField] private GameObject destinationObject;
-    private Transform _destination;
-
-    private NavMeshAgent _navMeshAgent;
-    void Start()
+    public void Awake()
     {
-        _destination = destinationObject.GetComponent<Transform>();
-        _navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+        _roamingFragment = BuildRoamingFragment();
+        _stateMachine = new StateMachine(_roamingFragment.Entry);
     }
 
-    void Update()
+    private void Start()
     {
-        if (Input.GetKeyUp(KeyCode.Return))
-        {
-            _navMeshAgent.destination = _destination.position;
-        }
-
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            Debug.Log(_navMeshAgent.isStopped);
-            Debug.Log(_navMeshAgent.destination.x + " " + _navMeshAgent.destination.y +
-                      " " + _navMeshAgent.destination.z);
-        }
+        _stateMachine.OnEnter();
     }
+
+    private void Update()
+    {
+        _stateMachine.Execute();
+    }
+
+    private StateMachineFragment BuildRoamingFragment()
+    {
+        var stayState = new State();
+        var followState = new State();
+
+        var timer = new CountDownTimer();
+        var stayAction = new StayAction(enemy, timer, 1.0f, 2.0f);
+
+        var stayToFollowDecision = new StayToFollowDecision(timer);
+        var stayToFollowTransition = new Transition(stayToFollowDecision, followState);
+
+        var followAction = new FollowAction(enemy, 1.0f, 2.0f);
+
+        var followToStayDecision = new FollowToStayDecision(enemy, 0.01f);
+        var followToStayTransition = new Transition(followToStayDecision, stayState);
+
+        stayState.AddAction(stayAction);
+        stayState.AddTransition(stayToFollowTransition);
+
+        followState.AddAction(followAction);
+        followState.AddTransition(followToStayTransition);
+
+        _roamingFragment = new StateMachineFragment(stayState);
+        _roamingFragment.AddState(followState);
+
+        return _roamingFragment;
+    }
+
+    [SerializeField] private GameObject enemy;
+
+    private StateMachine _stateMachine;
+    private StateMachineFragment _roamingFragment;
 }
