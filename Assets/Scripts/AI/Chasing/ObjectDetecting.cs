8e0b33343e18c83f54a15b0cba76c2ef5804d09f
vladnovoren <vladnovoren@gmail.com>
2022-11-11 15:03:22 +0300

    started object detecting

---
+++ Assets/Scripts/AI/Chasing/ObjectDetecting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace AI.Chasing
+{
+    public class ObjectDetecting
+    {
+        public ObjectDetecting(GameObject persecutor, GameObject victim)
+        {
+            _persecutor = persecutor;
+            _fov = persecutor.GetComponent<FieldOfView>();
+            
+            _victim = victim;
+            _victimWidth = victim.GetComponent<ObjectWidth>();
+        }
+
+        public bool TryDetect()
+        {
+        }
+
+        private GameObject _persecutor;
+        private FieldOfView _fov;
+        
+        private GameObject _victim;
+        private ObjectWidth _victimWidth;
+    }
+}
---
+++ Assets/Scripts/AI/Chasing/ToChaseDecision.cs
@@ -0,0 +1,19 @@
+﻿using AI.Base;
+using UnityEngine;
+
+namespace AI.Chasing
+{
+    public class ToChaseDecision : AGameObjectBasedDecision
+    {
+        public ToChaseDecision(GameObject owner, GameObject chased) : base(owner)
+        {
+            _chased = chased;
+        }
+
+        public override bool Decide()
+        {
+        }
+
+        private GameObject _chased;
+    }
+}
