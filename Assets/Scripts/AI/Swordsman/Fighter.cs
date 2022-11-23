3c2e1fa6cbbfb0bed9076b48e7bf73dc0c50bafb
rocaruler <vladnovoren@gmail.com>
2022-11-23 12:58:48 +0300

    raw version of non-MonoBehaviour AI.Swordsman.Fighter

--- Assets/Scripts/AI/Swordsman/Fighter.cs
+++ Assets/Scripts/AI/Swordsman/Fighter.cs
@@ -1,30 +1,50 @@
 using UnityEngine;
 using Utils.Time;
+using Lifetime;
 
 namespace AI.Swordsman
 {
-    public class Fighter : MonoBehaviour
+    public class Fighter
     {
-        private void Start()
+        public Fighter(GameObject owner, GameObject enemy, float reloadTime)
         {
             _timer = new CountDownTimer();
             _timer.Restart(0.0f);
             _sword = gameObject.GetComponent<Sword>();
+            _enemyHealth = enemy.GetComponent<Health>();
         }
 
-        public float ReloadTime { get; set; } = 1.0f;
-        public GameObject Enemy;
-
-        public float TryHit()
+        public void TryHit()
         {
-            if (!_timer.IsDown())
-                return 0.0f;
-
-            _timer.Restart(ReloadTime);
-            return _sword.Damage;
+            if (CanHit())
+            {
+                _enemyHealth.TakeSwordDamage(_sword.Damage);
+                _timer.Restart();
+            }
         }
+
+        private bool CanHit()
+        {
+            return _timer.IsDown() && CheckRaycast();
+        }
+
+        private bool CheckRaycast()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_transform.position, _transform.forward,
+                                out hit))
+                if (hit.transform.gameObject == _enemy)
+                    return true;
+            return false;
+        }
+
+        private Transform _transform;
+
+        private GameObject _enemy;
+        private Health _enemyHealth;
 
         private Sword _sword;
+        private float _reloadTime;
         private CountDownTimer _timer;
     }
 }
